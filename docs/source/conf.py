b23095ff3f2435bbb38fcb94a741205888dea625
Gagis00 <119961616+Gagis00@users.noreply.github.com>
2024-05-22 11:23:15 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Ingenieros Jr'
+copyright = '2024, Raúl Antonio'
+author = 'Gagis'
 
 release = '0.1'
 version = '0.1.0'
